b6f54d5ceb828c0f9a429eefada035424edf71c6
angelgpaz <anpaz0912@gmail.com>
2024-04-18 12:17:19 -0400

    LeetProblemEasy completed

--- Group08_FinalProject/LeetProblemEasy.aspx.cs
+++ Group08_FinalProject/LeetProblemEasy.aspx.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace Group08_FinalProject
 {
-    public partial class LeetProblemEasy : System.Web.UI.Page
+    public partial class LeetProblemEasy : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
