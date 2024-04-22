6a2d451a5ad5ad565c94f5d8263ae08a55a08a55
Abdoulch197 <165812403+Abdoulch197@users.noreply.github.com>
2024-04-22 15:05:37 -0400

    Revert "completed Leetcode easy"

--- Group08_FinalProject/LeetProblemEasy.aspx.cs
+++ Group08_FinalProject/LeetProblemEasy.aspx.cs
@@ -1,55 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace Group08_FinalProject
 {
-    public partial class LeetProblemEasy : Page
+    public partial class LeetProblemEasy : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // You can perform any initialization here if needed
-        }
 
-        protected void CalculateButton_Click(object sender, EventArgs e)
-        {
-            // Parse input values and calculate Two Sum
-            string[] numStrings = NumsTextBox.Text.Split(',');
-            int[] nums = Array.ConvertAll(numStrings, int.Parse);
-            int target = int.Parse(TargetTextBox.Text);
-
-            Solution solution = new Solution();
-            try
-            {
-                int[] result = solution.TwoSum(nums, target);
-                OutputLiteral.Text = $"Output: [{result[0]}, {result[1]}]";
-            }
-            catch (ArgumentException ex)
-            {
-                OutputLiteral.Text = ex.Message;
-            }
-        }
-
-        public class Solution
-        {
-            public int[] TwoSum(int[] nums, int target)
-            {
-                Dictionary<int, int> numIndices = new Dictionary<int, int>();
-
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    int complement = target - nums[i];
-                    if (numIndices.ContainsKey(complement))
-                    {
-                        return new int[] { numIndices[complement], i };
-                    }
-                    if (!numIndices.ContainsKey(nums[i]))
-                    {
-                        numIndices.Add(nums[i], i);
-                    }
-                }
-                throw new ArgumentException("No two sum solution");
-            }
         }
     }
 }
